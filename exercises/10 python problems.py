47dfadb4ceb6869dde3561c9e80faee181d9ad50
Maggie Ma <maggiema.tx@gmail.com>
2023-12-31 08:21:24 -0600

    Dec 31 due

--- exercises/10 python problems.py
+++ exercises/10 python problems.py
@@ -23,3 +23,31 @@ else:
     print("Not Prime")
 
 #  exercise 4: 
+x=[1,2,3,4,5]
+while x:
+    print(x.pop(0))
+
+# exercise 5: 
+arr=np.arange(0,10)
+arr
+
+# exercise 6:
+arr_reshaped=arr.reshap((2,5))
+arr_reshaped
+
+# exercise 7:
+l1=[1,2,3]
+l2=[4,5,6]
+print(l1+l2)
+
+# exercise 8:
+matrix1=np.arrange(1,10).reshape(3,3)
+pring(matrix1)
+
+# exercise 9:
+matrix2=np.zeros([3,5])
+matrix2
+
+# exercise 10:
+arr_num=[5,4,6,7,3]
+bp_arr_num=np.array(arr_num)
